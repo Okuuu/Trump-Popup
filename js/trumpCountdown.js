daee2793c68a1e4cd4871553ad0796cd99830b20
Okuuu <robin.mesa-marin@eemi.com>
2016-11-15 13:00:29 +0100

    First commit

---
+++ js/trumpCountdown.js
@@ -0,0 +1,76 @@
+jQuery( function( $ ) {
+    $("body").append( '<div id="trumpPopup">' +
+        '<div id ="text">' +
+        'Dear Fellow American, you have to endure Donald Trump as President for still ' +
+        '<br>' +
+        '<span id="days"></span> Days ' +
+        '<span id="hours"></span> hours ' +
+        '<span id="minutes"></span> minutes and  ' +
+        '<span id="seconds"></span> seconds</div>' +
+        '</div>' +
+        '</div>' +
+        '<style>@import url("https://fonts.googleapis.com/css?family=Raleway");' +
+        '#trumpPopup{' +
+        'background-image: url("https://media.giphy.com/media/Bnu7lQjaXZUVa/giphy.gif");' +
+        'background-repeat: no-repeat;' +
+        'opacity: 0.8; width: 730px; height:100%;' +
+        '}' +
+        '#text{' +
+        'position: relative;' +
+        'top:20px;' +
+        'margin: 0%;' +
+        'font-family: "Raleway", sans-serif;' +
+        'color: white;' +
+        'font-size: 1.5rem;' +
+        'text-align: center;' +
+        '}' +
+        '#text span{' +
+        'font-size: 2.5rem;' +
+        '}' +
+        '</style>')
+    $("#trumpPopup").dialog({
+        title: '',
+        hide : 0,
+        modal: true,
+        width:700,
+        height:500
+    })
+})
+
+var deadline = '2020-11-08'; //This is the end time of the countdown, can be change, not sure about the date tho, I just picked the same date as 2016 ... But in 2020 ;p
+
+
+function getTimeRemaining(endtime){
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor( (t/1000) % 60 );
+    var minutes = Math.floor( (t/1000/60) % 60 );
+    var hours = Math.floor( (t/(1000*60*60)) % 24 );
+    var days = Math.floor( t/(1000*60*60*24) );
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+//not sure of this tho ..
+
+function initializeClock(id, endtime){
+    var clock = document.getElementById(id);
+    var timeinterval = setInterval(function(){
+        var t = getTimeRemaining(endtime);
+
+        days.innerHTML= t.days;
+        hours.innerHTML= t.hours;
+        minutes.innerHTML= t.minutes;
+        seconds.innerHTML= t.seconds;
+
+        if(t.total<=0){
+            clearInterval(timeinterval);
+        }
+    },1000);
+}
+
+initializeClock('clockdiv', deadline);
---
+++ trumpCountdown.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: Plugin which countdown the end of the mandate of Donald J Trump
+Plugin URI:  https://developer.wordpress.org/plugins/the-basics/
+Description: This plugin will display a countdown which display for how long our dear fellow americans have to deal with Donald Trump, just a stupid feature, don't take it too seriously ;p
+Version:   000000001
+Author:    Robin Mesa Marin
+Author URI:  https://developer.wordpress.org/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wporg
+Domain Path: /plugin1/plugin1.php
+*/
+
+defined( 'ABSPATH' )
+or die ( 'No direct load !' );
+
+/**
+ * useful constants for plugin
+ **/
+define( 'PLUGINTRUMP', plugin_dir_url( __FILE__ ) );
+define( 'PLUGINTRUMP_VERSION', '0.1' );
+
+/**
+ * No need for explanation here => self explanatory
+ * @author Julien Maury
+ */
+function _trumpPlugin_enqueue_scripts(){
+    wp_enqueue_script( 'pluginTrump', PLUGINTRUMP . 'js/trumpCountdown.js', [ 'jquery-ui-dialog' ], PLUGINTRUMP_VERSION, true );
+    wp_enqueue_style( 'wp-jquery-ui-dialog' );
+}
+
+add_action( 'wp_enqueue_scripts', '_trumpPlugin_enqueue_scripts' );
